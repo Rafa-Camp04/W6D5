346d4c279e5c6daa7b18895e207917a17db67130
Rafael Campos <rafaelcampos04@hotmail.com>
2024-04-12 15:27:39 -0400

    Starting Phase 1

---
+++ ninety-nine-cats/app/models/cat.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+
+    CAT_COLORS = ["orange", "white", "black", "beige", "gray"]
+
+    validates :birth_date, :name, presence: true
+    validates :color, presence: true, inclusion: { in: CAT_COLORS }
+    validates :sex, presence: true, inclusion: { in: ["M", "F"] }
+    validate :birth_date_cannot_be_future
+
+    def birth_date_cannot_be_future
+        if birth_date.present? && birth_date > Date.today
+            errors.add(:birth_date, "can't be in the future")
+        end
+    end 
+
+end
